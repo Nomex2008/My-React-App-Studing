62c476914275765d954aaa96f00e99fdcef4026c
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-28 23:49:52 +0200

    Commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
 import styles from './app.module.css'
 
+const NAME = [
+  'Alex',
+  'Nomex',
+]
+
 function App() {
+  let [count, setCount] = useState(0)
+
   return (
     <section className={styles.app}>
       <p>Hi!</p>
+      <div>
+      I am {NAME.map(name => (<span key={name}>{name} </span>))}
+      </div>
+      <span className={styles.span}>Number:  </span><button onClick={() => setCount((count) => count + 1)}>{count}</button>
     </section>
   )
 }
