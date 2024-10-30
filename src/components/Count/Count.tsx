c0428ac51b8953492632fb922a01775645affd5c
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-10-30 23:56:14 +0200

    Commit 4(setDetails)
    Add setDetails

--- src/components/Count/Count.tsx
+++ src/components/Count/Count.tsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import styles from '../../app.module.css'
 
 const Count = () => {
-    let [count, setCount] = useState(0)
+    const [details, setDetails] = useState({
+        count: 0
+    })
 
     return (
     <>
         <div className={styles.count}>
-            <span className={styles.span}>Number:  </span><button onClick={() => setCount((count) => count + 1)}>{count}</button>
+            <span className={styles.span}>Number:  </span><button onClick={() => setDetails((details) => {
+                return {
+                    count: details.count + 1
+                }
+            })}>{details.count}</button>
         </div>
 
-        <div onClick={() => setCount(count => count = 0)} className={styles.restart}>RESTART!</div>
+        {//<div onClick={() => setCount(count => count = 0)} className={styles.restart}>RESTART!</div>
+        }
     </>
     )
 }
