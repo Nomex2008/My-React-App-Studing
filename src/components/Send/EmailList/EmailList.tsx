2075c7465c0fa00789c6ddd8e4b6ada12fdca8b1
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-11-09 23:50:54 +0200

    Commit 14(Send2)

---
+++ src/components/Send/EmailList/EmailList.tsx
@@ -0,0 +1,7 @@
+const EmailList = () => {
+  return (
+    <div>EmailList</div>
+  )
+}
+
+export default EmailList
--- src/components/Send/Send.tsx
+++ src/components/Send/Send.tsx
@@ -1,9 +1,32 @@
+import { Bold, Eraser, Italic, Underline } from "lucide-react"
 import { Link } from "react-router-dom"
+import EmailList from "./EmailList/EmailList"
 import styles from './send.module.css'
 
 const Send = () => {
+  let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam recusandae aliquam, aspernatur quo eius quod dolorem et veniam? Beatae doloribus totam tenetur ad doloremque nobis quo repellat itaque vel et?'
+
   return (
     <section className={styles.send}>
+
+      <h1 className={styles.title}>Email editor</h1>
+
+      <div className={styles.card}>
+        <textarea className={styles.editor} spellCheck='false' 
+        value={text}
+        onChange={() => {}}/>
+        <div className={styles.action}>
+          <div className={styles.tools}>
+            <button><Eraser/></button>
+            <button><Bold/></button>
+            <button><Italic/></button>
+            <button><Underline/></button>
+          </div>
+          <button className={styles.button}>Send now</button>
+        </div>
+      </div>
+
+      <div className={styles.emailList}><EmailList/></div>
 
       <div className={styles.return}><Link to='/'>Return</Link></div>
     </section>
